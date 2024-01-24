53ef2911068a00e5608653a8a522eb97ce15d55c
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-24 15:13:47 +0900

    chore: prepare nix dev env

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    purs-overlay = {
+      url = "github:thomashoneyman/purescript-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, purs-overlay, ... }:
+  flake-utils.lib.eachDefaultSystem (system:
+  let
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [
+        purs-overlay.overlays.default
+      ];
+    };
+  in
+  {
+    devShells.default = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        esbuild
+        nodejs
+        purs
+        purs-backend-es
+        spago-unstable
+        purs-tidy
+      ];
+    };
+  });
+}
