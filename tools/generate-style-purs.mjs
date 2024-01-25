5234ef4ca75a691f554b04cf664150fbcf2b8985
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-25 18:23:40 +0900

    docs: Chalk.Style

--- tools/generate-style-purs.mjs
+++ tools/generate-style-purs.mjs
@@ -11,6 +11,21 @@ const allNames = modifierNames.concat(
     backgroundColorNames,
     [ "fg", "bg" ]
 );
+stdout.write("-- | Styles of modification/colors applied to the chalk.\n");
+stdout.write("-- |\n");
+stdout.write("-- | These styles are basically of function `Chalk -> Chalk`, appending another\n");
+stdout.write("-- | modification to the given chalk.\n");
+stdout.write("-- | The full list of styles can be found [here](https://github.com/chalk/chalk#styles)\n");
+stdout.write("-- | in the original README.\n");
+stdout.write("-- |\n");
+stdout.write("-- | An only difference from the original API is that those six methods to apply\n");
+stdout.write("-- | styles using RGB/ANSI256 color models, namely `chalk.rgb`, `chalk.hex`,\n");
+stdout.write("-- | `chalk.ansi256`, `chalk.bgRgb`, `chalk.bgHex`, and `chalk.bgAnsi256` are\n");
+stdout.write("-- | integrated into two functions `fg` and `bg`, thanks to Purescript's GADTs.\n");
+stdout.write("-- |\n");
+stdout.write("-- | ### See also\n");
+stdout.write("-- |\n");
+stdout.write("-- | - https://github.com/chalk/chalk#chalkstylestylestring-string\n");
 stdout.write("module Chalk.Style\n  ( Style\n  , ");
 stdout.write(allNames.join("\n  , "));
 stdout.write("\n  ) where\n\n");
@@ -51,10 +66,12 @@ stdout.write("foreign import _bgRgb :: Fn3 Int Int Int Style\n");
 stdout.write("foreign import _bgHex :: String -> Style\n");
 stdout.write("foreign import _bgAnsi256 :: Int -> Style\n\n");
 
+stdout.write("-- | Modify foreground color with the given color.\n");
 stdout.write("fg :: Color -> Style\n");
 stdout.write("fg (RGB { r, g, b }) = runFn3 _rgb r g b\n");
 stdout.write("fg (Hex hex) = _hex hex\n");
 stdout.write("fg (ANSI256 i) = _ansi256 i\n\n");
+stdout.write("-- | Modify background color with the given color.\n");
 stdout.write("bg :: Color -> Style\n");
 stdout.write("bg (RGB { r, g, b }) = runFn3 _bgRgb r g b\n");
 stdout.write("bg (Hex hex) = _bgHex hex\n");
