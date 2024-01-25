d2776ed4da737e224bfa8dd4fcabe973ea3dbb85
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-25 22:32:31 +0900

    docs: Chalk

--- src/Chalk.purs
+++ src/Chalk.purs
@@ -1,3 +1,16 @@
+-- | This module imports and re-exports some *default* submodules for convenience,
+-- | namely
+-- |
+-- | - `Chalk.Common`
+-- | - `Chalk.Color`
+-- | - `Chalk.Style`
+-- | - `Chalk.Stdout`
+-- |
+-- | The last one, `Chalk.Stdout`, might not be for your use case,
+-- | as it works in `Eff` and customized for the STDOUT stream.
+-- | To use chalk in `Aff`, import `Chalk.Stdout.Aff` or `Chalk.Stderr.Aff` instead.
+-- | To use chalk for the STDERR stream, import `Chalk.Stderr` or `Chalk.Stderr.Aff`
+-- | instead.
 module Chalk
   ( module Common
   , module Color
