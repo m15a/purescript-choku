03ecdb4706005510c696ed90747f515c63365bbf
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-25 22:49:33 +0900

    test: show example use cases

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -1,12 +1,65 @@
-module Test.Main where
+module Test.Main (main) where
 
-import Prelude
-
+import Prelude (Unit, (<>), (=<<))
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Console (log)
+
+import Chalk
 
 main :: Effect Unit
 main = do
-  log "🍕"
-  log "You should add some tests."
+
+  let
+    space = reset :& " "
+    cr = reset :& "\n"
+    end = reset :& ""
+
+  log =<< bold :& "bold"
+    <> space
+    <> dim :& "dim"
+    <> space
+    <> italic :& "italic"
+    <> space
+    <> underline :& "underline"
+    <> space
+    <> inverse :& "inverse"
+    <> space
+    <> strikethrough :& "strikethrough"
+    <> space
+    <> black :& "black"
+    <> cr
+
+    <> red :& "red"
+    <> space
+    <> green :& "green"
+    <> space
+    <> yellow :& "yellow"
+    <> space
+    <> blue :& "blue"
+    <> space
+    <> magenta :& "magenta"
+    <> space
+    <> cyan :& "cyan"
+    <> space
+    <> white :& "white"
+    <> space
+    <> gray :& "gray"
+    <> space
+    <> bgBlack :& "bgBlack"
+    <> cr
+
+    <> bgRed :& "bgRed"
+    <> space
+    <> bgGreen :& "bgGreen"
+    <> space
+    <> bgYellow :& "bgYellow"
+    <> space
+    <> bgBlue :& "bgBlue"
+    <> space
+    <> bgMagenta :& "bgMagenta"
+    <> space
+    <> bgCyan :& "bgCyan"
+    <> space
+    <> bgWhite :& "bgWhite"
+    <> end
 
