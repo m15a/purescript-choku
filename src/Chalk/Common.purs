939b9cc712f51ccab7d1c2bd1a14e680eda13324
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-25 17:24:45 +0900

    docs: Chalk.Common

--- src/Chalk/Common.purs
+++ src/Chalk/Common.purs
@@ -1,3 +1,4 @@
+-- | The chalk type and its common API.
 module Chalk.Common
   ( Chalk
   , level
@@ -11,8 +12,13 @@ module Chalk.Common
 import Prelude
 import Data.Maybe (Maybe(..))
 
+-- | The chalk type, simply wrapping a Javascript `Chalk` object.
 newtype Chalk = Chalk Unit
 
+-- | Return the color support level of the given chalk.
+-- |
+-- | Higher level supports more colors.
+-- | For detail, see https://github.com/chalk/chalk#chalklevel.
 foreign import level :: Chalk -> Int
 
 instance Eq Chalk where
@@ -26,11 +32,31 @@ instance Show Chalk where
 
 foreign import _newChalk :: Int -> Chalk
 
+-- | Create a new chalk with the specified level of color support.
+-- |
+-- | The level should be an integer bounded between `0` and `3`.
+-- | Giving an out-of-bounds integer produces `Nothing`.
 mkChalk :: Int -> Maybe Chalk
 mkChalk level_
   | level_ # between 0 3 = Just (_newChalk level_)
   | otherwise = Nothing
 
+-- | Modify a string with the chalk.
+-- |
+-- | For example,
+-- |
+-- | ```purescript
+-- | import Prelude
+-- | import Data.Maybe
+-- | import Partial.Unsafe
+-- | import Chalk
+-- | let
+-- |   chk = unsafePartial $ fromJust (mkChalk 3) # bold # cyan
+-- | in
+-- |   "hello" `withChalk` chk
+-- | ```
+-- |
+-- | produces a bold cyan `"hello"` string.
 withChalk :: String -> Chalk -> String
 withChalk = flip withChalkFlipped
 
