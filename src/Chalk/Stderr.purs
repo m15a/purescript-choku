c823d126bcb75e581bf6f0d192ec5a715c9a746d
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-25 18:02:25 +0900

    docs: Chalk.Std{out,err}

--- src/Chalk/Stderr.purs
+++ src/Chalk/Stderr.purs
@@ -1,3 +1,10 @@
+-- | Chalk in `Effect`, for the STDERR stream.
+-- |
+-- | See module `Chalk.Stdout` for more information.
+-- | 
+-- | ### See also
+-- |
+-- | - https://github.com/chalk/chalk#chalkstderr-and-supportscolorstderr
 module Chalk.Stderr
   ( chalk
   , supportsColor
@@ -12,8 +19,13 @@ import Effect (Effect)
 import Chalk.Common (Chalk, withChalk)
 import Chalk.Style (Style)
 
+-- | Get the chalk instance for STDERR, automatically detecting the level
+-- | of color support.
 foreign import chalk :: Effect Chalk
 
+-- | Provide detailed information of the color support level.
+-- |
+-- | See https://github.com/chalk/chalk#supportscolor.
 foreign import supportsColor
   :: Effect
        { level :: Int
@@ -22,6 +34,7 @@ foreign import supportsColor
        , has16m :: Boolean
        }
 
+-- | Modify a string, using the chalk for STDERR, with the given style.
 withStyle :: String -> Style -> Effect String
 withStyle s style = do
   c <- chalk
--- src/Chalk/Stdout.purs
+++ src/Chalk/Stdout.purs
@@ -1,3 +1,17 @@
+-- | Chalk in `Effect`, for the STDOUT stream.
+-- | 
+-- | The default chalk instance automatically detect the level of color support
+-- | in the environment, either STDOUT or STDERR.
+-- | This module provides the chalk instance for the STDOUT stream.
+-- |
+-- | For the STDERR stream, look at module `Chalk.Stderr`.
+-- |
+-- | NOTE: You can force the color support level via environment variable
+-- | `$FORCE_COLOR`.
+-- |
+-- | ### See also
+-- |
+-- | - https://github.com/chalk/chalk#supportscolor
 module Chalk.Stdout
   ( chalk
   , supportsColor
@@ -12,8 +26,13 @@ import Effect (Effect)
 import Chalk.Common (Chalk, withChalk)
 import Chalk.Style (Style)
 
+-- | Get the default chalk instance for STDOUT, automatically detecting the level
+-- | of color support.
 foreign import chalk :: Effect Chalk
 
+-- | Provide detailed information of the color support level.
+-- |
+-- | See https://github.com/chalk/chalk#supportscolor.
 foreign import supportsColor
   :: Effect
        { level :: Int
@@ -22,6 +41,21 @@ foreign import supportsColor
        , has16m :: Boolean
        }
 
+-- | Modify a string, using the default chalk, with the given style.
+-- |
+-- | For example,
+-- |
+-- | ```purescript
+-- | import Prelude
+-- | import Effect.Console
+-- | import Data.Maybe
+-- | import Partial.Unsafe
+-- | import Chalk
+-- |
+-- | log =<< "hello" `withStyle` (inverse >>> underline)
+-- | ```
+-- |
+-- | prints an inversed and underlined `"hello"` string.
 withStyle :: String -> Style -> Effect String
 withStyle s style = do
   c <- chalk
