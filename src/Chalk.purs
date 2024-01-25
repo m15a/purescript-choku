58741cc140182453a3423d2ca8118e943eabd273
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-01-25 16:14:30 +0900

    feat: export basic modules

---
+++ src/Chalk.purs
@@ -0,0 +1,11 @@
+module Chalk
+  ( module Common
+  , module Color
+  , module Style
+  , module Stdout
+  ) where
+
+import Chalk.Common as Common
+import Chalk.Color as Color
+import Chalk.Style as Style
+import Chalk.Stdout as Stdout
