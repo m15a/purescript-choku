b9447b89866ecd30c13755e7a9d473ee8a84feea
NACAMURA Mitsuhiro <m15@m15a.dev>
2024-02-10 20:04:33 +0900

    test: add level 3 tests

--- test/Test/Main.purs
+++ test/Test/Main.purs
@@ -7,7 +7,9 @@ import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner (runSpec)
 
 import Test.Level0 (level0Spec)
+import Test.Level3 (level3Spec)
 
 main :: Effect Unit
 main = launchAff_ $ runSpec [ consoleReporter ] do
   level0Spec
+  level3Spec
